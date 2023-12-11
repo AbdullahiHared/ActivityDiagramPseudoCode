68ff7ed83589d0e99611483a34fd5348f1e6a5db
Abdullahi <muxsinxareed6@gmail.com>
2023-12-11 09:41:13 +0100

    Pseudo Code for menu Program

--- src/activityDiagramCode.java
+++ src/activityDiagramCode.java
@@ -19,6 +19,33 @@ public class activityDiagramCode {
         }
 
 
+        // Second Exercise
+
+
+        System.out.println("First Num? ");
+        int firstNumber = sc.nextInt();
+
+        if(firstNumber > 10) {
+            System.out.println("The number is larger than 10");
+        } else {
+            System.out.println("The number is equal to or less than 10");
+        }
+
+        // PseudoCode d
+
+        /*
+           Print out choices : Menu program: 1. Print out temperature, 2. Wind
+           Declare an integer (value) = the value is chosen by the user through the scanner.
+           if value is 1
+           print : "It is 9 C"
+
+           if else value is 2
+           print "Det är 9 second-meter wind"
+
+           else print "Wrong"
+         */
+
+
 
 
     }
